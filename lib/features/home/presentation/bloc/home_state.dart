feb72b4f01a07895fd94f113ac0e3a79cbe001b7
Hadel Brmo <brmohadel56@gmail.com>
2026-06-12 16:17:12 +0300

    git commit -m "feat: enhance HomeScreen functionality
    - Integrated TextField search with FetchHomeServingsEvent.
    - Added advance filtering via BottomSheet for PaymentUnit.
    - Implemented infinite scroll pagination using ScrollController.
    - Adapted UI components and PaidServicePage for dark and light themes."

---
+++ lib/features/home/presentation/bloc/home_state.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+import '../../domain/entities/service_entity.dart';
+
+abstract class HomeState extends Equatable {
+  const HomeState();
+  @override
+  List<Object?> get props => [];
+}
+
+class HomeInitialState extends HomeState {}
+
+class HomeLoadingState extends HomeState {}
+
+class HomeSuccessState extends HomeState {
+  final List<ServicingEntity> servings;
+  const HomeSuccessState({required this.servings});
+
+  @override
+  List<Object?> get props => [servings];
+}
+
+class HomeErrorState extends HomeState {
+  final String message;
+  const HomeErrorState({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
